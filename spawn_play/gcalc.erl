cd57409e91c03763c48e3b44c5f42de03dda33e2
HIROE Shin <shin@ubuntu01.(none)>
2009-11-09 13:21:23 +0900

    modify

--- spawn_play/gcalc.erl
+++ spawn_play/gcalc.erl
@@ -1,30 +1,34 @@
 -module(gcalc).
 -compile(export_all).
 
+%%リモートでプロセスを立ち上げる。
 start(Node) ->
-    ping(Node),                %%リモートのノードと接続を確立する。
-    shell_default:nl(?MODULE), %%モジュールをリモートのノードに送信する。
-    spawn(Node, fun() -> loop() end).
+    ping(Node),                       %%リモートのノードと接続を確立する。
+    shell_default:nl(?MODULE),        %%モジュールをリモートのノードに送信する。
+    spawn(Node, fun() -> loop() end). %%ノードを指定してプロセスを立ち上げ。
 
+%%start/1の名前登録版。他のホストのノードからも登録した名前で処理を依頼できるようになる。
 start(Node, Name) ->
     global:register_name(Name, start(Node)).
 
 rpc(Pid, Request) ->
-
     %% Pidが登録された名前でもPid番号でも同様に動作するように。
     case is_atom(Pid) of
 	true -> global:send(Pid, {self(), Pid, Request});
 	false -> Pid ! {self(), Pid, Request}
     end,
     
+    %%リモートからの処理結果を受信する。
     receive
 	{Pid, Response} -> Response;
 	Any -> io:format("received: ~p~n", [Any])
     end.
 
+%%リモートとの接続を確立。
 ping(Node) ->
     net_kernel:connect_node(Node).
 
+%%リモートの処理リクエスト待ちのループ。
 loop() ->
     receive
 	{From, Name, {plus, X, Y}} ->
@@ -45,8 +49,10 @@ loop() ->
 	    loop()
     end.
 
+%%足算するだけの関数。
 plus(X, Y) ->
     X + Y.
 
+%%引算するだけの関数。
 minus(X, Y) ->
     X - Y.
