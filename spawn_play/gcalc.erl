56ea032a566d330d49eca078987085f8bed4fae2
HIROE Shin <shin@ubuntu01.(none)>
2009-11-09 17:10:48 +0900

    modify for cluster

--- spawn_play/gcalc.erl
+++ spawn_play/gcalc.erl
@@ -1,11 +1,27 @@
 -module(gcalc).
 -compile(export_all).
+-import(lists).
+
+init() ->
+    case ets:info(clusters) of
+	undefined ->
+	    ets:new(clusters, [private, named_table, bag]);
+	_ -> none_exec
+    end.
+
 
 %%リモートでプロセスを立ち上げる。
 start(Node) ->
-    ping(Node),                       %%リモートのノードと接続を確立する。
-    shell_default:nl(?MODULE),        %%モジュールをリモートのノードに送信する。
-    spawn(Node, fun() -> loop() end). %%ノードを指定してプロセスを立ち上げ。
+    init(),
+
+    ping(Node),                             %%リモートのノードと接続を確立する。
+    shell_default:nl(?MODULE),              %%モジュールをリモートのノードに送信する。
+
+    Pid = spawn(Node, fun() -> loop() end), %%ノードを指定してプロセスを立ち上げ。
+
+    ets:insert(clusters, {?MODULE, Pid}),   %%Pid番号を保存しておく。
+    Pid.
+
 
 %%start/1の名前登録版。他のホストのノードからも登録した名前で処理を依頼できるようになる。
 start(Node, Name) ->
@@ -24,19 +40,38 @@ rpc(Pid, Request) ->
 	Any -> io:format("received: ~p~n", [Any])
     end.
 
+rpc(Request) ->
+    Pid = get_pid(),
+    rpc(Pid, Request).
+
 %%リモートとの接続を確立。
 ping(Node) ->
     net_kernel:connect_node(Node).
+
+get_pid() ->
+    case ets:lookup(clusters, ?MODULE) of
+	[] -> none_clusters;
+
+	[{?MODULE, Pid}] -> 
+	    io:format("calling to ~p~n", [Pid]),
+	    Pid;
+	Clusters ->
+	    Index = random:uniform(length(Clusters)),
+	    {?MODULE, Pid} = lists:nth(Index, Clusters),
+	    io:format("calling to ~p~n", [Pid]),
+	    Pid
+    end.
+    
 
 %%リモートの処理リクエスト待ちのループ。
 loop() ->
     receive
 	{From, Name, {plus, X, Y}} ->
-	    From ! {Name, plus(X, Y)},
+	    From ! {Name, remote_plus(X, Y)},
 	    loop();
 
 	{From, Name, {minus, X, Y}} ->
-	    From ! {Name, minus(X, Y)},
+	    From ! {Name, remote_minus(X, Y)},
 	    loop();
 
 	{From, Name, Any} ->
@@ -50,9 +85,15 @@ loop() ->
     end.
 
 %%足算するだけの関数。
-plus(X, Y) ->
+remote_plus(X, Y) ->
     X + Y.
 
+plus(X, Y) ->
+    rpc({plus, X, Y}).
+
 %%引算するだけの関数。
-minus(X, Y) ->
+remote_minus(X, Y) ->
     X - Y.
+
+minus(X, Y) ->
+    rpc({minus, X, Y}).
