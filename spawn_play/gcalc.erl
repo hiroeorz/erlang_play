1607f5edcbe0a4e94b418f34f7eac314c173a4e9
HIROE Shin <shin@ubuntu01.(none)>
2009-11-09 13:15:21 +0900

    modify

--- spawn_play/gcalc.erl
+++ spawn_play/gcalc.erl
@@ -2,16 +2,20 @@
 -compile(export_all).
 
 start(Node) ->
-    ping(Node),
-    shell_default:nl(?MODULE),
-
+    ping(Node),                %%リモートのノードと接続を確立する。
+    shell_default:nl(?MODULE), %%モジュールをリモートのノードに送信する。
     spawn(Node, fun() -> loop() end).
 
 start(Node, Name) ->
     global:register_name(Name, start(Node)).
 
 rpc(Pid, Request) ->
-    global:send(Pid, {self(), Pid, Request}),
+
+    %% Pidが登録された名前でもPid番号でも同様に動作するように。
+    case is_atom(Pid) of
+	true -> global:send(Pid, {self(), Pid, Request});
+	false -> Pid ! {self(), Pid, Request}
+    end,
     
     receive
 	{Pid, Response} -> Response;
